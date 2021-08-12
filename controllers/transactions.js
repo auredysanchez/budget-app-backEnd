76df172b5a24ba079314d2fd87784bf3d3a4f705
Auredy Sanchez <auredysanchez@gmail.com>
2021-08-11 23:03:17 -0400

    backend routes all connected; working

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,12 +1,11 @@
 const transactions = require("express").Router();
 const transactionsArray = require("../models/transaction");
-// const express = require("express")
-
+const express = require("express");
 
 //index
-transactions.get("/", (req,res) => {
-    res.json(transactionsArray)
-})
+transactions.get("/", (req, res) => {
+  res.status(200).json(transactionsArray);
+});
 
 // transactions.get("/:arrayIndex", (req,res) => {
 //     const {arrayIndex} =req.params
@@ -17,9 +16,8 @@ transactions.get("/", (req,res) => {
 transactions.get("/:arrayIndex", (req, res) => {
   const { arrayIndex } = req.params;
   const transaction = transactionsArray[arrayIndex];
-  // bookmark and all its details...
   if (transaction) {
-    res.json(transaction);
+    res.status(200).json(transaction);
   } else {
     res.redirect("/404");
   }
@@ -27,26 +25,25 @@ transactions.get("/:arrayIndex", (req, res) => {
 
 //create
 transactions.post("/", (req, res) => {
-    const { body } = req;
-    transactionsArray.push(body);
-    const newIndx = transactionsArray.length - 1;
-    res.json(transactionsArray[newIndx]);
-  });
+  const { body } = req;
+  transactionsArray.push(body);
+  const newIndex = transactionsArray.length - 1;
+  res.json(transactionsArray[newIndex]);
+});
 
-  //update
-transactions.put("/:arrayIndex", (req, res) => {
-    const { arrayIndex } = req.params;
-    const { body } = req;
-    transactionsArray[arrayIndex] = body;
-    res.json(transactionsArray[arrayIndex]);
-  }); 
-
+//update
+// transactions.put("/:arrayIndex", (req, res) => {
+//   const { arrayIndex } = req.params;
+//   const { body } = req;
+//   transactionsArray[arrayIndex] = body;
+//   res.json(transactionsArray[arrayIndex]);
+// });
 
 //delete
-  transactions.delete("/:arrayIndex", (req, res) => {
-    const { arrayIndex } = req.params; 
-    const deletedTransaction = transactionsArray.splice(arrayIndex, 1);
-    res.json(deletedTransaction[0]);
-  });
+transactions.delete("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  const deleteTransaction = transactionsArray.splice(arrayIndex, 1);
+  res.status(200).json(deleteTransaction[0]);
+});
 
-  module.exports = transactions;
+module.exports = transactions;
